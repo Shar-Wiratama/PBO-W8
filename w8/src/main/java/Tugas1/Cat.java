e375a153793ded9ed991852343780318bcb996aa
Shar-Wiratama <80728579+Shar-Wiratama@users.noreply.github.com>
2021-11-13 05:05:06 +0700

    Add files via upload

---
+++ w8/src/main/java/Tugas1/Cat.java
@@ -0,0 +1,12 @@
+package Tugas1;
+
+class Cat extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Meow");
+    }
+    public static void main(String args[]){
+        Animal obj = new Cat();
+        obj.sound();
+    }
+}
